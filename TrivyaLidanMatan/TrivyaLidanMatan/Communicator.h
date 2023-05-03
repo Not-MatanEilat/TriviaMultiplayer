1ffe1b627fc372b36a06659b924096e986a60639
MatanEilat <matangourion@gmail.com>
2023-05-03 15:12:18 +0300

    Added fields for Communicator - m_serverSocket, m_clients m_sereverSocket - The main server socket m_clients - a map that has a key of their socket and a value of an IRequestHandler

--- TrivyaLidanMatan/TrivyaLidanMatan/Communicator.h
+++ TrivyaLidanMatan/TrivyaLidanMatan/Communicator.h
@@ -1,7 +1,18 @@
 #pragma once
+
+
+#include <WinSock2.h>
+#include <map>
+
+#include "IRequestHandler.h"
+
 class Communicator
 {
 public:
 	void startHandleRequests();
+
+private:
+	SOCKET m_serverSocket;
+	std::map<SOCKET, IRequestHandler*> m_clients;
 };
 
