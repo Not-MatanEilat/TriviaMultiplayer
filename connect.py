6224f3327c0ff8e782d9d5c9808c9b45252ee63c
Lidan <ls301205@gmail.com>
2023-05-06 16:13:28 +0300

    added comments to connect.py

--- connect.py
+++ connect.py
@@ -4,12 +4,14 @@ SERVER_PORT = 8826
 BYTES_SIZE = 1024 
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', SERVER_PORT))
-    msg = client.recv(BYTES_SIZE)
-    print(msg.decode())
-    client.send('Hello'.encode())
-    client.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        # connect to the server
+        client.connect(('localhost', SERVER_PORT))
+        # receive the message from the server and print it
+        msg = client.recv(BYTES_SIZE)
+        print(msg.decode())
+        # send a message to the server
+        client.send('Hello'.encode())
 
 if __name__ == '__main__':
     main()
