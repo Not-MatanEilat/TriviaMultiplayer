afd697124dc3c588aee5c4a59bc69489d3399625
MatanEilat <matangourion@gmail.com>
2023-05-06 20:37:06 +0300

    Added a function serializeResponse(LoginResponse) in the class JsonResponsePacketSerizaler The function will take a signup response and serialize it to a string ("status" : "status")

--- TrivyaLidanMatan/TrivyaLidanMatan/JsonResponsePacketSerializer.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/JsonResponsePacketSerializer.cpp
@@ -14,5 +14,18 @@ string JsonResponsePacketSerializer::serializeResponse(ErrorResponse errorRespon
 
 }
 
+/**
+ * \brief The function will take a signup response and serialize it to a string ("status" : "status")
+ * \param loginResponse The login response to serialize
+ * \return login response serialized to a string
+ */
+string JsonResponsePacketSerializer::serializeResponse(LoginResponse loginResponse)
+{
+	json j;
+	j["status"] = loginResponse.status;
+	return j.dump();
+}
+
+
 
 
