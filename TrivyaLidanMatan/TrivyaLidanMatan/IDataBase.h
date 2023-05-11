34465d6bf6ff89f4d7d2865847a78f8fd9dd22d9
MatanEilat <matangourion@gmail.com>
2023-05-11 16:45:09 +0300

    Added a new interface for the DataBase with the following pure functions Open - opens the database and returns true if succeeded - false if it didn't Close - closes the database and returns true if succeeded - false if it didn't doesUserExist doesPasswordMatch addNewUser

---
+++ TrivyaLidanMatan/TrivyaLidanMatan/IDataBase.h
@@ -0,0 +1,13 @@
+#pragma once
+class IDataBase
+{
+	virtual bool open() = 0;
+	virtual bool close() = 0;
+	virtual int doesUserExist(string username) = 0;
+	virtual int doesPasswordMatch(string username, string password) = 0;
+	virtual int addNewUser(string username, string password, string email) = 0;
+
+
+
+};
+
