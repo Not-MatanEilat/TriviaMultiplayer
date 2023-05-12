f25bc68231fb3bf8053c57894c0b9abc874aa070
MatanEilat <matangourion@gmail.com>
2023-05-12 15:27:04 +0300

    Added the function login to LoginManager The function will login the given user to the database, throws an excetion if the user doesn't exists, also adds the user to the list of logged users

--- TrivyaLidanMatan/TrivyaLidanMatan/LoginManager.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/LoginManager.cpp
@@ -25,3 +25,28 @@ void LoginManager::signUp(string const& username, string const& password, string
 	m_database->addNewUser(username, password, email);
 }
 
+/**
+ * \brief The function will login the given user to the database, throws
+ * an excetion if the user doesn't exists, also adds the user to the list of logged users
+ * \param username the username to login
+ * \param password the password to login
+ */
+void LoginManager::login(string const& username, string const& password)
+{
+	if (!m_database->doesUserExist(username))
+	{
+		throw std::exception("User doesn't exist");
+	}
+
+	if (!m_database->doesPasswordMatch(username, password))
+	{
+		throw std::exception("Password is incorrect");
+
+	}
+
+	// add the user to the logged users
+	LoggedUser const user(username);
+	m_loggedUsers.push_back(user);
+}
+
+
