e4708b0101dd070b83fe28b449b4fce03eadda02
Lidan <ls301205@gmail.com>
2023-05-14 18:50:47 +0300

    added parse_message method to Parse a message from the server. added create_client_socket to Create a client socket and connect to the server. added send_request to Send a request to the server and return the response. added login to Send a login request to the server and return the response. added signup to Send a signup request to the server and return the response. the main method tests login and signup

--- connect.py
+++ connect.py
@@ -6,6 +6,7 @@ BYTES_SIZE = 1024
 LOGIN_CODE = 1
 SIGNUP_CODE = 2
 
+
 def build_message(code: int, message: str) -> bytes:
     """
     Build a message to send to the server.
@@ -16,52 +17,99 @@ def build_message(code: int, message: str) -> bytes:
     :return: bytes
     """
     return (
-        code.to_bytes(1, "big")
-        + len(message).to_bytes(4, "big")
-        + message.encode()
+            code.to_bytes(1, "big")
+            + len(message).to_bytes(4, "big")
+            + message.encode()
     )
 
+
+def parse_message(message: bytes) -> dict:
+    """
+    Parse a message from the server.
+    :param message: bytes
+    :return: dict
+    """
+    code = int.from_bytes(message[:1], "big")
+    msg_len = int.from_bytes(message[1:5], "big")
+    msg = message[5:].decode()
+    return {"code": code, "len": msg_len, "msg": msg}
+
+
+def create_client_socket() -> socket.socket:
+    """
+    Create a client socket and connect to the server.
+    :return: socket
+    """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("localhost", SERVER_PORT))
+    msg = client.recv(BYTES_SIZE)  # skip the welcome message
+    return client
+
+
+def send_request(client: socket.socket, message: bytes) -> bytes:
+    """
+    Send a request to the server and return the response.
+    :param client: client socket
+    :param message: message to send to the server
+    :return: None
+    """
+    client.send(message)
+    return client.recv(BYTES_SIZE)
+
+
+def login(client: socket.socket, username: str, password: str) -> bytes:
+    """
+    Send a login request to the server and return the response.
+    :param client: client socket
+    :param username: username
+    :param password: password
+    :return: None
+    """
+    message = '{"username": "' + username + '","password": "' + password + '"}'
+    bin_message = build_message(LOGIN_CODE, message)
+    return send_request(client, bin_message)
+
+
+def signup(client: socket.socket, username: str, password: str, email: str) -> bytes:
+    """
+    Send a signup request to the server and return the response.
+    :param client: client socket
+    :param username: username
+    :param password: password
+    :param email: email
+    :return: None
+    """
+    message = '{"username": "' + username + '","password": "' + password + '","email": "' + email + '"}'
+    bin_message = build_message(SIGNUP_CODE, message)
+    return send_request(client, bin_message)
+
+
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        # connect to the server
-        client.connect(("localhost", SERVER_PORT))
-        # receive the message from the server and print it
-        msg = client.recv(BYTES_SIZE)
-        print(msg.decode())
+    # test signup
+    client = create_client_socket()
+    print(signup(client, "test", "test", "test@mail.com"))
 
-        message = '{"username": "user1","password": "1234"}'
+    # test signup with existing username
+    client.close()
+    client = create_client_socket()
+    print(signup(client, "test", "test", "test@mail.com"))
 
-        # build the message by the format CODE(1 BYTE) | MESSAGE_LEN (4 BYTES) | MESSAGE
-        bin_message = build_message(LOGIN_CODE, message)
+    # test login
+    client.close()
+    client = create_client_socket()
+    print(login(client, "test", "test"))
 
-        # send a message to the server
-        client.send(bin_message)
+    # test login with wrong password
+    client.close()
+    client = create_client_socket()
+    print(login(client, "test", "wrong"))
 
-        # get message fom the server
-        msg = client.recv(BYTES_SIZE)
-        print(msg)
+    # test login with wrong username
+    client.close()
+    client = create_client_socket()
+    print(login(client, "wrong", "test"))
 
-        message = '{"username": "user1","password": "1234","email": "user1@gmail.com"}'
 
-        # build a signup message now this time
-        bin_message = build_message(SIGNUP_CODE, message)
-
-        # send a message to the server
-        client.send(bin_message)
-
-        # get message fom the server
-        msg = client.recv(BYTES_SIZE)
-        print(msg)
-
-        # build an invalid message now this time
-        bin_message = build_message(123, "{NOOOOOOOOOO}")
-
-        # send a message to the server
-        client.send(bin_message)
-
-        # get message fom the server
-        msg = client.recv(BYTES_SIZE)
-        print(msg)
 
 
 if __name__ == "__main__":
