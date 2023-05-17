36f6a8b37d5998d47a6aa237fbfbc5743fc368ba
MatanEilat <matangourion@gmail.com>
2023-05-17 17:31:26 +0300

    Added function addUser to the Room class The function will add a  user to the current room

--- TrivyaLidanMatan/TrivyaLidanMatan/Room.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/Room.cpp
@@ -9,3 +9,13 @@ Room::Room(const RoomData& metadata): m_metadata(metadata)
 		
 }
 
+/**
+ * \brief Function will add a user to the current room
+ * \param user the user to add
+ */
+void Room::addUser(LoggedUser user)
+{
+	m_users.push_back(user);
+}
+
+
