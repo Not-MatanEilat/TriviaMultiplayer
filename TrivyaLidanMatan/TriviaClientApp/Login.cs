bd998ca189536907389d2f597637ec10ea93310b
MatanEilat <matangourion@gmail.com>
2023-05-24 17:43:30 +0300

    Added the Signup Form

--- TrivyaLidanMatan/TriviaClientApp/Login.cs
+++ TrivyaLidanMatan/TriviaClientApp/Login.cs
@@ -11,13 +11,6 @@ namespace TriviaClientApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            TriviaClient client = TriviaClient.GetClient();
-            var res = client.Login("lidan", "error12#$");
-            Dictionary<string, object> msg = res["message"] as Dictionary<string, object>;
-            if (msg != null)
-            {
-                Debug.WriteLine(msg["status"]);
-            }
             
         }
     }
---
+++ TrivyaLidanMatan/TriviaClientApp/Signup.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriviaClientApp
+{
+    public partial class Signup : Form
+    {
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+
+            TriviaClient client = TriviaClient.GetClient();
+
+            label1 = new Label();
+            usernameTextBox = new TextBox();
+            passwordTextBox = new TextBox();
+            emailTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(398, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Signup";
+            label1.Click += label1_Click;
+            // 
+            // usernameTextBox
+            // 
+            usernameTextBox.Location = new Point(398, 199);
+            usernameTextBox.Name = "usernameTextBox";
+            usernameTextBox.Size = new Size(100, 23);
+            usernameTextBox.TabIndex = 1;
+            usernameTextBox.TextChanged += textBox1_TextChanged_1;
+            // 
+            // passwordTextBox
+            // 
+            passwordTextBox.Location = new Point(398, 240);
+            passwordTextBox.Name = "passwordTextBox";
+            passwordTextBox.Size = new Size(100, 23);
+            passwordTextBox.TabIndex = 2;
+            passwordTextBox.TextChanged += passwordTextBox_TextChanged;
+            // 
+            // emailTextBox
+            // 
+            emailTextBox.Location = new Point(398, 282);
+            emailTextBox.Name = "emailTextBox";
+            emailTextBox.Size = new Size(100, 23);
+            emailTextBox.TabIndex = 2;
+            emailTextBox.TextChanged += passwordTextBox_TextChanged;
+            // 
+            // Signup
+            // 
+            ClientSize = new Size(920, 435);
+            Controls.Add(emailTextBox);
+            Controls.Add(passwordTextBox);
+            Controls.Add(usernameTextBox);
+            Controls.Add(label1);
+            Name = "Signup";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
