a4eac22dbb1fc9ee153822650383ff6a8a702f7f
MatanEilat <matangourion@gmail.com>
2023-05-27 20:48:22 +0300

    Added function isRequestRelevant to the RoomAdminRequestHandler The function checks if the given request is relevant to the handler

--- TrivyaLidanMatan/TrivyaLidanMatan/RoomAdminRequestHandler.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/RoomAdminRequestHandler.cpp
@@ -1,1 +1,16 @@
 #include "RoomAdminRequestHandler.h"
+
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h""
+
+/**
+ * \brief Checks if the given request is relevant to the handler
+ * \param info the request info given
+ * \return True or False
+ */
+bool RoomAdminRequestHandler::isRequestRelevant(RequestInfo info)
+{
+	int code = info.requestId;
+	return code == CLOSE_ROOM_CODE || code == ROOM_STATE_CODE || code == START_GAME_CODE;
+}
+
