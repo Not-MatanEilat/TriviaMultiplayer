bb83fb8de8137c236e894cb63bc05a8b69d812bd
Lidan <ls301205@gmail.com>
2023-05-28 13:19:18 +0300

    implemented handleRequest in RoomMemberRequestHandler

--- TrivyaLidanMatan/TrivyaLidanMatan/RoomMemberRequestHandler.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/RoomMemberRequestHandler.cpp
@@ -1,5 +1,7 @@
 #include "RoomMemberRequestHandler.h"
 
+#include "JsonResponsePacketSerializer.h"
+
 RoomMemberRequestHandler::RoomMemberRequestHandler(RequestHandlerFactory& handlerFactory, const LoggedUser& user, const Room& room) :
 	m_room(room), m_user(user), m_roomManager(handlerFactory.getRoomManager()), m_handlerFactory(handlerFactory)
 {
@@ -9,4 +11,39 @@ bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo info)
 {
 	int code = info.requestId;
 	return code == LEAVE_ROOM_CODE || code == ROOM_STATE_CODE;
+}
+
+RequestResult RoomMemberRequestHandler::handleRequest(RequestInfo info)
+{
+	RequestResult result;
+	result.newHandler = this;
+	try
+	{
+		if (info.requestId == LEAVE_ROOM_CODE)
+		{
+			result = leaveRoom(info);
+		}
+		else if (info.requestId == ROOM_STATE_CODE)
+		{
+			result = getRoomState(info);
+		}
+	}
+	catch (const std::exception& e)
+	{
+		ErrorResponse error;
+		error.message = "Something went wrong: " + std::string(e.what());
+		TRACE("MenuHandler " << m_user.getUsername() << ": " << error.message)
+			result.response = JsonResponsePacketSerializer::serializeResponse(error);
+		result.newHandler = this;
+	}
+	return result;
+}
+
+RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo info)
+{
+
+}
+
+RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo info)
+{
 }
