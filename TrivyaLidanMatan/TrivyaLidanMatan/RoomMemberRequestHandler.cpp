5e19c15f2b207b69338bda56720b0037407cb7dc
Lidan <ls301205@gmail.com>
2023-05-28 13:16:27 +0300

    implemented isRequestRelevant in RoomMemberRequestHandler

--- TrivyaLidanMatan/TrivyaLidanMatan/RoomMemberRequestHandler.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/RoomMemberRequestHandler.cpp
@@ -1,1 +1,12 @@
 #include "RoomMemberRequestHandler.h"
+
+RoomMemberRequestHandler::RoomMemberRequestHandler(RequestHandlerFactory& handlerFactory, const LoggedUser& user, const Room& room) :
+	m_room(room), m_user(user), m_roomManager(handlerFactory.getRoomManager()), m_handlerFactory(handlerFactory)
+{
+}
+
+bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo info)
+{
+	int code = info.requestId;
+	return code == LEAVE_ROOM_CODE || code == ROOM_STATE_CODE;
+}
