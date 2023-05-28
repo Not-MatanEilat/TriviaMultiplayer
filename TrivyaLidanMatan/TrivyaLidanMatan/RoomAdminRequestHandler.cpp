7fd385f3fad79dc27da019500048906459e8f148
MatanEilat <matangourion@gmail.com>
2023-05-28 13:09:38 +0300

    Added function handleRequest in the RoomAdminRequestHandler class The function will handle the admin user of the current room

--- TrivyaLidanMatan/TrivyaLidanMatan/RoomAdminRequestHandler.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/RoomAdminRequestHandler.cpp
@@ -1,7 +1,7 @@
 #include "RoomAdminRequestHandler.h"
 
 #include "JsonRequestPacketDeserializer.h"
-#include "JsonResponsePacketSerializer.h""
+#include "JsonResponsePacketSerializer.h"
 
 /**
  * \brief Checks if the given request is relevant to the handler
@@ -13,4 +13,43 @@ bool RoomAdminRequestHandler::isRequestRelevant(RequestInfo info)
 	int code = info.requestId;
 	return code == CLOSE_ROOM_CODE || code == ROOM_STATE_CODE || code == START_GAME_CODE;
 }
+
+/**
+ * \brief The function will handle the admin user of the current room
+ * \param info the info of request
+ * \return the result for request
+ */
+RequestResult RoomAdminRequestHandler::handleRequest(RequestInfo info)
+ {
+	int code = info.requestId;
+	RequestResult result;
+	try
+	{
+		if (code == CLOSE_ROOM_CODE)
+		{
+			result = closeRoom(info);
+		}
+		else if (code == ROOM_STATE_CODE)
+		{
+			result = getRoomState(info);
+		}
+		else if (code == START_GAME_CODE)
+		{
+			result = startGame(info);
+		}
+	}
+	catch (const std::exception& e)
+	{
+		ErrorResponse error;
+	 	error.message = "Something went wrong: " + std::string(e.what());
+	 	TRACE("MenuHandler " << m_user.getUsername() << ": " << error.message)
+	 	result.response = JsonResponsePacketSerializer::serializeResponse(error);
+	 	result.newHandler = this;
+	}
+
+	return result;
+ }
+
+
+
 
