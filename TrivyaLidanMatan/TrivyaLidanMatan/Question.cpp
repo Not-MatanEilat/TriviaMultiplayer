159be7ce3c2f7a57dc0f3e076368f97c4eafc8d8
Lidan <ls301205@gmail.com>
2023-06-04 22:58:31 +0300

    added getPossibleAnswers in Question to return the possible answers

--- TrivyaLidanMatan/TrivyaLidanMatan/Question.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/Question.cpp
@@ -13,3 +13,12 @@ string Question::getQuestion() const
 {
 	return m_question;
 }
+
+/**
+ * \brief return the possible answers
+ * \return the possible answers
+ */
+vector<string> Question::getPossibleAnswers() const
+{
+	return m_possibleAnswers;
+}
