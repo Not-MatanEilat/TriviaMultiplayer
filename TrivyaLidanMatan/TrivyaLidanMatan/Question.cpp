61960eb49ba597a4da856b978af563ca99c2b37c
Lidan <ls301205@gmail.com>
2023-06-04 22:54:58 +0300

    added Question class headers

--- TrivyaLidanMatan/TrivyaLidanMatan/Question.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/Question.cpp
@@ -1,1 +1,6 @@
 #include "Question.h"
+
+Question::Question(const string& question, const vector<string>& possibleAnswers): m_question(question),
+	m_possibleAnswers(possibleAnswers)
+{
+}
--- TrivyaLidanMatan/TrivyaLidanMatan/Question.h
+++ TrivyaLidanMatan/TrivyaLidanMatan/Question.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "IDataBase.h"
+
+
 class Question
 {
+public:
+	Question(const string& question, const vector<string>& possibleAnswers);
+
+	string getQuestion() const;
+	vector<string> getPossibleAnswers() const;
+	int getCorrectAnswerId() const;
+private:
+	string m_question;
+	vector<string> m_possibleAnswers;
 };
 
