8a22c2cdc67cb25f1a83023984d0d77132b69916
Lidan <ls301205@gmail.com>
2023-06-04 23:47:08 +0300

    added submitAnswer to submits the answer of the user

--- TrivyaLidanMatan/TrivyaLidanMatan/Game.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/Game.cpp
@@ -1,6 +1,25 @@
 #include "Game.h"
 
 /**
+ * \brief submits the answer of the user
+ * \param loggedUser user
+ * \param answerId answer id
+ */
+void Game::submitAnswer(const LoggedUser& loggedUser, unsigned answerId)
+{
+	GameData gameData = m_players[loggedUser];
+	if (answerId == gameData.currentQuestion.getCorrectAnswerId())
+	{
+		gameData.correctAnswerCount++;
+	}
+	else
+	{
+		gameData.wrongAnswerCount++;
+	}
+	
+}
+
+/**
  * \brief returns the id of the game
  * \return game id
  */
