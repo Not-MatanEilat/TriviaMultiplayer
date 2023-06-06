716d046fbb37627e6ced8a132fca4364bc8b20b9
Lidan <ls301205@gmail.com>
2023-06-07 01:15:20 +0300

    updated getQuestion.py to decode the html encoding and the questions now display correctly

--- getQuestions.py
+++ getQuestions.py
@@ -5,19 +5,44 @@ import sqlite3
 
 DB_PATH = "TrivyaLidanMatan\\TrivyaLidanMatan\\Trivia.db"
 
+def custom_unescape(string: str) -> str:
+    """
+    Custom unescape for the html
+    :param string: the string to unescape
+    :return: the unescaped string
+    """
+    # do something with the unescaped string
+    string = unescape(string)
+    string = string.replace('"', '""')
+    return string
+
+def custom_decoder(dct: dict) -> dict:
+    """
+    Custom decoder for the json
+    :param dct: the dictionary to decode
+    :return: the decoded dictionary
+    """
+    # do something with the decoded dictionary
+    if "question" in dct:
+        dct["question"] = custom_unescape(dct["question"])
+        dct["correct_answer"] = custom_unescape(dct["correct_answer"])
+        dct["incorrect_answers"] = [custom_unescape(answer) for answer in dct["incorrect_answers"]]
+    return dct
 
 def main():
-    amount = 50
-
     # get answers from API
-    answer = requests.get(f'https://opentdb.com/api.php?amount={amount}&category=15&difficulty=medium&type=multiple')
+    answer = requests.get(f'https://opentdb.com/api.php?amount=50&type=multiple')
 
     # connect the db and make a cursor object (object used to execute)
     connect = sqlite3.connect(DB_PATH)
     cursor = connect.cursor()
 
+    response = answer.text
+    # response = response.replace('"', '""')
+
     # make the answer into a json
-    answer_json = json.loads(answer.text)
+    answer_json = json.loads(response, object_hook=custom_decoder)
+
 
     # iterate all questions and put each one of the inside of the data base
     for result in answer_json["results"]:
@@ -25,21 +50,19 @@ def main():
         question = result["question"]
 
         # quotes are being passed as "quot;" in the question so we replace those
-        decoded_question = unescape(question)
-        decoded_question = decoded_question.replace('"', '""')
-
+        
         correct_answer = result["correct_answer"]
         incorrect_answers = result["incorrect_answers"]
 
         # the line to execute
-        execute_line = f'INSERT INTO questions VALUES ("{decoded_question}", "{correct_answer}", "{incorrect_answers[0]}", "{incorrect_answers[1]}", "{incorrect_answers[2]}")'
+        execute_line = f'INSERT INTO questions VALUES ("{question}", "{correct_answer}", "{incorrect_answers[0]}", "{incorrect_answers[1]}", "{incorrect_answers[2]}")'
 
         # execute and then commit
         try:
             cursor.execute(execute_line)
             connect.commit()
         except sqlite3.IntegrityError:
-            print("Question already exists " + decoded_question)
+            print("Question already exists " + question)
 
     # after, close the database
     connect.close()
