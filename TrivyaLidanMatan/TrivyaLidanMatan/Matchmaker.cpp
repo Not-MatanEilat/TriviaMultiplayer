d6b129d4ae60cbb52ab58055341bbff091c0a697
MatanEilat <matangourion@gmail.com>
2023-06-11 01:56:28 +0300

    Added constructor to Matchmaker

--- TrivyaLidanMatan/TrivyaLidanMatan/Matchmaker.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/Matchmaker.cpp
@@ -1,1 +1,12 @@
 #include "Matchmaker.h"
+
+/**
+ * \brief The constructor for the Matchmaker class.
+ * \param playersPerGame amount of players per game
+ * \param roomManager ref from room manager
+ */
+Matchmaker::Matchmaker(int playersPerGame, RoomManager& roomManager) : m_playersPerGame(playersPerGame), m_roomManager(roomManager)
+{
+	
+}
+
