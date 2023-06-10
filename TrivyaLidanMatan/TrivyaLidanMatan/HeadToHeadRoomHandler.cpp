38d1218b808b47976900c2daaadb413b32c69319
MatanEilat <matangourion@gmail.com>
2023-06-10 22:08:35 +0300

    Added function handleRequest to HeadToHeadRoomHandler Handles the Head To Head room

--- TrivyaLidanMatan/TrivyaLidanMatan/HeadToHeadRoomHandler.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/HeadToHeadRoomHandler.cpp
@@ -23,3 +23,38 @@ bool HeadToHeadRoomHandler::isRequestRelevant(RequestInfo info)
 	int code = info.requestId;
 	return code == LEAVE_ROOM_CODE || code == ROOM_STATE_CODE;
 }
+
+/**
+ * \brief Handles the Head To Head room
+ * \param info the info of request
+ * \return the result of request
+ */
+RequestResult HeadToHeadRoomHandler::handleRequest(RequestInfo info)
+{
+	RequestResult result;
+	checkForGameStart();
+
+	try
+	{
+		if (info.requestId == LEAVE_ROOM_CODE)
+		{
+			result = leaveRoom(info);
+		}
+		else if (info.requestId == ROOM_STATE_CODE)
+		{
+			result = getRoomState(info);
+		}
+
+	}
+	catch (const std::exception& e)
+	{
+		ErrorResponse error;
+		error.message = "Something went wrong: " + std::string(e.what());
+		TRACE("HeadToHeadRequestHandler " << m_user.getUsername() << ": " << error.message)
+			result.response = JsonResponsePacketSerializer::serializeResponse(error);
+		result.newHandler = m_handlerFactory.createMenuRequestHandler(m_user);
+
+	}
+
+	return result;
+}
