27dd14b1e2c4edfffd43ca792af6b42d6d5d1293
Lidan <ls301205@gmail.com>
2023-06-30 02:23:45 +0300

    improved the setup file

--- setup.bat
+++ setup.bat
@@ -1,19 +1,28 @@
 @echo off
-if exist "C:\vcpkg" (
-  echo Already installed vcpkg
-  cd C:\vcpkg
-  echo Installing cryptopp
-  vcpkg install cryptopp
-) else (
+goto :main
+
+REM Function to install vcpkg if it is not already installed
+:install_vcpkg
   echo Installing vcpkg
   cd C:\
   git clone https://github.com/Microsoft/vcpkg.git
   cd vcpkg
-  bootstrap-vcpkg.bat
+  cmd /c bootstrap-vcpkg.bat
   vcpkg integrate install
+goto :install_cryptopp
 
-  cd C:\vcpkg
+REM Function to install cryptopp
+:install_cryptopp
   echo Installing cryptopp
   vcpkg install cryptopp
+goto :eof
 
+:main
+REM Check if vcpkg is already installed
+if exist "C:\vcpkg" (
+  echo Already installed vcpkg
+  cd C:\vcpkg
+  call :install_cryptopp
+) else (
+  call :install_vcpkg
 )
