c01c943340808df7403c840538e25d16ade52095
Lidan <ls301205@gmail.com>
2023-07-01 23:30:18 +0300

    removed Cryptopp as it is not used in this version removed setup.bat as it is not used in this version

--- TrivyaLidanMatan/TrivyaLidanMatan/main.cpp
+++ TrivyaLidanMatan/TrivyaLidanMatan/main.cpp
@@ -1,16 +1,12 @@
 #pragma comment (lib, "ws2_32.lib")
 
-#include "OTPCryptoAlgorithm.h"
 #include "WSAInitializer.h"
 #include "Server.h"
-#include <cryptopp/aes.h>
 
 int main()
 {
 	WSAInitializer wsaInit;
 	SqliteDataBase db;
-	OTPCryptoAlgorithm otp(db);
-	ICryptoAlgorithm::testEncyption(db, otp);
 
 	Server server(&db);
 	server.run();
